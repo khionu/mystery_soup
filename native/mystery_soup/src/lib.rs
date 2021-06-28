f85831193d48ce217973384f71c14497e85bafd2
Khionu Sybiern <dev@khionu.net>
2021-06-27 17:58:27 -0700

    Remove resource wrapper

--- native/mystery_soup/src/lib.rs
+++ native/mystery_soup/src/lib.rs
@@ -1,7 +1,7 @@
 use randomize::PCG32;
-use rustler::{resource, Env, Error, NifResult, NifStruct, ResourceArc, Term};
+use rustler::{resource, Env, Error, NifResult, NifStruct, Term};
 
-#[derive(NifStruct)]
+#[derive(NifStruct, Clone)]
 #[module = "MysterySoup.PCG32"]
 struct RngState {
     state: u64,
@@ -9,34 +9,34 @@ struct RngState {
 }
 
 #[rustler::nif]
-fn init_state() -> NifResult<ResourceArc<RngState>> {
+fn init_state() -> NifResult<RngState> {
     let mut arr = [0_u64, 0];
     if getrandom::getrandom(bytemuck::bytes_of_mut(&mut arr)).is_err() {
         return Err(Error::Atom("sys_rand_err"));
     }
     let [state, inc] = arr;
-    Ok(ResourceArc::new(RngState { state, inc }))
+    Ok(RngState { state, inc })
 }
 
 #[rustler::nif]
-fn next(term_state: ResourceArc<RngState>) -> (u32, ResourceArc<RngState>) {
+fn next(term_state: RngState) -> (u32, RngState) {
     next_prv(term_state)
 }
 
-fn next_prv(term_state: ResourceArc<RngState>) -> (u32, ResourceArc<RngState>) {
-    let RngState { state, inc } = *term_state;
+fn next_prv(term_state: RngState) -> (u32, RngState) {
+    let RngState { state, inc } = term_state;
     let mut rng = PCG32 { state, inc };
     (
         rng.next_u32(),
-        ResourceArc::new(RngState {
+        RngState {
             state: rng.state,
             inc: rng.inc,
-        }),
+        },
     )
 }
 
 #[rustler::nif]
-fn next_float(term_state: ResourceArc<RngState>) -> (f32, ResourceArc<RngState>) {
+fn next_float(term_state: RngState) -> (f32, RngState) {
     let (mut bit_src, mut term_state) = next_prv(term_state);
     let mut bit_count = 0;
 
@@ -65,7 +65,7 @@ fn next_float(term_state: ResourceArc<RngState>) -> (f32, ResourceArc<RngState>)
     (f32::from_bits(result), term_state)
 }
 
-fn get_bit(term_state: &mut ResourceArc<RngState>, bits: &mut u32, i: &mut u32) -> u32 {
+fn get_bit(term_state: &mut RngState, bits: &mut u32, i: &mut u32) -> u32 {
     if *i == 0 {
         let (new_bits, new_state) = next_prv(term_state.clone());
 
